9962fe77d9fcdc863ef92b24c5308fd556889652
Diego Peixoto <contato@diegopeixoto.net>
2018-06-16 17:59:19 -0300

    random via ajax

--- outerspace/src/random.js
+++ outerspace/src/random.js
@@ -1,10 +1,41 @@
 axios.get('https://code.diegopeixoto.net/outerspace/src/topics.json')
   .then(function (response) {
-    console.log(response.data.topicos.link);
+        var topicos = response.data.topicos;
+        gerarTopicoAleatorio(topicos);
+    
   })
   .catch(function (error) {
     console.log(error);
   });
 
 
+  function gerarTopicoAleatorio(topicos) {
+      const topico = Math.round(Math.random() * (topicos.length - 1));
+    abrirTopico('https://'+topicos[topico].link);      
+  }
 
+
+
+  function abrirTopico(link) {
+    $.ajax({
+      url: link,
+      method: 'POST',
+      succes: function (data) {
+        $('#content').html(data);
+      }
+    })
+  }
+
+  // function abrirUrlAjax(link){
+    
+  //   $.ajax({ 
+  //    url: link,
+  //     method: 'GET',      
+  //   success: function (data) {
+  //      setTimeout(function() { 
+  //             $('#content').html(data);
+         
+  //    });  
+               
+         
+  //   }
