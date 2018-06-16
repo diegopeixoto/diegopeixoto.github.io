fa4e5008ad076f9021dea07293e3ea46e3366f58
Diego Peixoto <contato@diegopeixoto.net>
2018-06-16 17:24:54 -0300

    changed to link

--- outerspace/src/random.js
+++ outerspace/src/random.js
@@ -1,8 +1,10 @@
 axios.get('https://code.diegopeixoto.net/outerspace/src/topics.json')
   .then(function (response) {
-    console.log(response);
+    console.log(response.data.topicos.link);
   })
   .catch(function (error) {
     console.log(error);
   });
+
+
 
