9c888d75c9f53e14f84302a1eeda82c907f8de3b
Diego Peixoto <contato@diegopeixoto.net>
2018-06-17 21:52:53 -0300

    removed https bypass // added fancy logs  Diego Peixoto committed

--- outerspace/random_topic/src/random.js
+++ outerspace/random_topic/src/random.js
@@ -14,7 +14,7 @@ axios.get('https://code.diegopeixoto.net/outerspace/random_topic/src/topics.json
       console.log ('Achamos esses tópicos: '+topicos);
       const topico = Math.round(Math.random() * (topicos.length - 1));
       console.log ('Vamos abrir este tópico aqui:'+topicos[topico].link);
-      abrirTopico('topicos[topico].link);      
+      abrirTopico('topicos[topico].link');      
   }
 
 
